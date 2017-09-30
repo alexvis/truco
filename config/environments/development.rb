4c05d9295c62ccf5d9ebadded7c0aad7038aa910
Alex Visconde <andres.visconde@gmail.com>
2017-09-30 19:35:43 -0400

    heroku add gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,24 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'helloteacherapp@gmail.com',
+  password:             'Cheto1950',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
+  config.action_mailer.default_url_options = {
+    host: "localhost",
+    port: 3000
+  }
+
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,21 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'helloteacherapp@gmail.com',
+  password:             'Cheto1950',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
+  config.action_mailer.default_url_options = {
+    host: "hello-teacher.herokuapp.com"
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
